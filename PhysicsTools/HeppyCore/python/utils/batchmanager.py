950e47afafeaf53356a87ba4024b1c5b3f61b976
cesare borgia <prinzvinz@gmx.at>
2019-10-25 11:17:36 +0200

    adding slurm batch option (still needs a fix for the job name -- $SLURMJOBID)

--- PhysicsTools/HeppyCore/python/utils/batchmanager.py
+++ PhysicsTools/HeppyCore/python/utils/batchmanager.py
@@ -282,6 +282,7 @@ class BatchManager:
         "LXPLUS-LSF" : batch command is bsub, and logged on lxplus
         "LXPLUS"     : batch command is condor, and logged on lxplus
         "PSI"        : batch command is qsub, and logged to t3uiXX
+        "PSI_SLURM   : batch command is sbatch, and logged to t3ui07
         "NAF"        : batch command is qsub, and logged on naf
         "IC"         : batch command is qsub, and logged on hep.ph.ic.ac.uk
         "LOCAL"      : batch command is nohup.
@@ -330,6 +331,16 @@ class BatchManager:
             else:
                 print('running on CONDOR (simple shared-filesystem version) : %s from %s' % (batchCmd, hostName))
                 return 'LXPLUS-CONDOR-SIMPLE'
+
+        ## VS 10/8/19 NEW ##################################################
+        elif batchCmd == "sbatch":
+            if onPSI:
+                print('running on SLURM : %s from %s' % (batchCmd, hostName))
+                return 'PSI_SLURM'
+            else:
+                err = 'Cannot run %s on %s' % (batchCmd, hostName)
+                raise ValueError( err )
+        ## VS 10/8/19 NEW ##################################################
 
         elif batchCmd == "qsub":
             if onPSI:
